f807ae0e0725e9fb6ca73bcf251e13622e3db746
Yuri Aksenov <aksenov.mailbox@gmail.com>
2017-10-18 12:42:20 +0300

    Увеличение картинки при клике по ней

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,20 @@ $(".read-fragment_button").click(function() {
 $(".popup--read-fragment__close").click(function() {
   $(".popup-wrapper").removeClass("popup-wrapper--active");
 });
+
+
+/* Увеличение картинки при клике по ней */
+
+$(".certificate__image img").click(function() {
+  $(".image-overlay").toggleClass("image-overlay--active");
+  $(".image-increase").toggleClass("image-increase--active");
+  $(".image-increase").prepend($(this).clone());
+});
+
+$(".image-overlay").click(function() {
+  $(".image-overlay").toggleClass("image-overlay--active");
+  $(".image-increase").toggleClass("image-increase--active");
+  $(".image-increase img").remove();
+});
+
+/* END - Увеличение картинки при клике по ней */
